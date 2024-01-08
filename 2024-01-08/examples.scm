b46552706aee9612556c383d5be00a40b7982df9
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2024-01-08 11:36:44 -0600

    Scripted auto-commit on change (2024-01-08 11:36:44) by gitwatch.sh

--- 2024-01-08/examples.scm
+++ 2024-01-08/examples.scm
@@ -5,3 +5,9 @@
 	#t
 	(and (number? (car lst))
 	     (list-of-numbers? (cdr lst))))))
+
+;;; (remove-first 2 '(9 2 3 2 5)) ---> (9 3 2 5)
+(define remove-first
+  (lambda (item lst)
+    (if (null? lst)
+	lst
