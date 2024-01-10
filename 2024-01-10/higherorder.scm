0abc071b5870f70cfd9aa589f66ef6e8a0a0f007
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2024-01-10 11:54:17 -0600

    Scripted auto-commit on change (2024-01-10 11:54:17) by gitwatch.sh

--- 2024-01-10/higherorder.scm
+++ 2024-01-10/higherorder.scm
@@ -7,3 +7,12 @@
 (map addOne '(1 2 3 4 5))
 
 (map (lambda (x) (+ x 1)) '(1 2 3 4 5))
+
+;;; Common task
+;;; Add 1 to every item in the list
+;;; sum up all the results
+;;; second part is the "reduce"
+;;; In Scheme, reduce <---> fold
+
+(use-modules ((rnrs)
+              #:select (fold-left fold-right)))
