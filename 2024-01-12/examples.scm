353a0f5cd1335e7b091b1e999187408fd909a71f
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2024-01-12 12:14:34 -0600

    Scripted auto-commit on change (2024-01-12 12:14:34) by gitwatch.sh

---
+++ 2024-01-12/examples.scm
@@ -0,0 +1,9 @@
+(define gen-lazy-list
+  (lambda (start stop)
+    (if (> start stop)
+        '()
+        (cons start
+            (lambda ()
+              (gen-lazy-list
+               (+ start 1)
+               stop))))))
