5739f94f3a05625b586b4965ed81265a290f6467
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2024-01-17 12:09:28 -0600

    Scripted auto-commit on change (2024-01-17 12:09:28) by gitwatch.sh

--- 2024-01-17/container_example2.c
+++ 2024-01-17/container_example2.c
@@ -10,7 +10,7 @@ struct Container {
 int main() {
    struct Container *w1 = malloc(sizeof(struct Container));
    struct Container *w2 = malloc(sizeof(struct Container));
-   printf("%i\n", w1);
+   //printf("%i\n", w1);
    (*w1).value = 1;
    w2 = w1;
    printf("%i %i\n",(*w1).value,(*w2).value);
