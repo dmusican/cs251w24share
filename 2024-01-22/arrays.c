3114fa3137f1761c561dbef6edc4daaef1bf8d7e
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2024-01-22 11:17:02 -0600

    Scripted auto-commit on change (2024-01-22 11:17:02) by gitwatch.sh

--- 2024-01-22/arrays.c
+++ 2024-01-22/arrays.c
@@ -1,2 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+int main() {
+    // Make an array
+    int a[3];
+    for (int i=0; i < 3; i++) {
+        a[i] = i;
+    }
+
+    for (int i=0; i < 3; i++) {
+        printf("a[%i] = %i\n", i, a[i]);
+    }
+}
