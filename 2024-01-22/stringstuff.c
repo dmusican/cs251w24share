d0d3704cb98f2e96256df65a3abc1171e91f7b9d
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2024-01-22 11:50:55 -0600

    Scripted auto-commit on change (2024-01-22 11:50:55) by gitwatch.sh

--- 2024-01-22/stringstuff.c
+++ 2024-01-22/stringstuff.c
@@ -7,6 +7,19 @@ int main() {
     char *word = "hello";
     word[0] = 'g';
 
+    // dull way to fix this problem
+    char *dullword = malloc(sizeof(char)*6);
+    dullword[0] = 'h';
+    dullword[1] = 'e';
+    dullword[2] = 'l';
+    dullword[3] = 'l';
+    dullword[4] = 'o';
+    dullword[5] = '\0';
+
+    printf("%s\n", dullword);
+    dullword[2] = 'q';
+    printf("%s\n", dullword);
+
 
 
 }
