4c76b4a703703f43f50eed9e70bb7c10d3701b0b
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2024-01-24 11:10:15 -0600

    Scripted auto-commit on change (2024-01-24 11:10:15) by gitwatch.sh

---
+++ 2024-01-24/linkedlist.c
@@ -0,0 +1,18 @@
+#include "linkedlist.h"
+#include <stdlib.h>
+
+LinkedList *insertFrontLL(LinkedList *list, int item) {
+    LinkedList *cell = malloc(sizeof(LinkedList));
+    (*cell).value = item;
+    (*cell).next = list;
+    return cell;
+
+}
+
+
+int main() {
+    LinkedList *list = NULL;
+    for (int i=0; i < 5; i++) {
+        list = insertFrontLL(list, i);
+    }
+}
