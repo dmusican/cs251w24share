93e91dc16c7af36fe9d0559b03ce1b243cf70341
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2024-02-16 12:41:58 -0600

    Scripted auto-commit on change (2024-02-16 12:41:58) by gitwatch.sh

---
+++ 2024-02-16/LambdaExample2.java
@@ -0,0 +1,11 @@
+// How passing functions as a parameter work in Java
+public class LambdaExample {
+
+    public static void evaluate(f, int x, int y) {
+        f(x,y);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(evaluate(+, 3, 5));
+    }
+}
