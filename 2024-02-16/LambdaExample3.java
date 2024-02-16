210687ff1513a7957786928dc924ab736c16db6d
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2024-02-16 12:48:18 -0600

    Scripted auto-commit on change (2024-02-16 12:48:18) by gitwatch.sh

--- 2024-02-16/LambdaExample3.java
+++ 2024-02-16/LambdaExample3.java
@@ -3,18 +3,6 @@ interface DoAThing {
     int f(int x, int y);
 }
 
-class AddingThing implements DoAThing {
-    public int f(int x, int y) {
-        return x + y;
-    }
-}
-
-class MultiplyingThing implements DoAThing {
-    public int f(int x, int y) {
-        return x * y;
-    }
-}
-
 public class LambdaExample3 {
 
     public static int evaluate(DoAThing thing,
@@ -23,8 +11,10 @@ public class LambdaExample3 {
     }
 
     public static void main(String[] args) {
-        System.out.println(evaluate(new AddingThing(),
-                                    3, 5));
+        System.out.println(
+           evaluate(new AddingThing(),
+                    3, 5));
+
         System.out.println(evaluate(new MultiplyingThing(),
                                     3, 5));
     }
