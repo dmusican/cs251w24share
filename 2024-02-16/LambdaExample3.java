2df8101283e5fd24c9ec6e2421a8919f494367e6
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2024-02-16 12:47:40 -0600

    Scripted auto-commit on change (2024-02-16 12:47:40) by gitwatch.sh

---
+++ 2024-02-16/LambdaExample3.java
@@ -0,0 +1,31 @@
+// How passing functions as a parameter work in Java
+interface DoAThing {
+    int f(int x, int y);
+}
+
+class AddingThing implements DoAThing {
+    public int f(int x, int y) {
+        return x + y;
+    }
+}
+
+class MultiplyingThing implements DoAThing {
+    public int f(int x, int y) {
+        return x * y;
+    }
+}
+
+public class LambdaExample2 {
+
+    public static int evaluate(DoAThing thing,
+                               int x, int y) {
+        return thing.f(x,y);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(evaluate(new AddingThing(),
+                                    3, 5));
+        System.out.println(evaluate(new MultiplyingThing(),
+                                    3, 5));
+    }
+}
