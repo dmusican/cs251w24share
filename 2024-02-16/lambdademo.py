c15bd91f38642f6dca5b2d39c8013c296443d5f2
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2024-02-16 12:20:58 -0600

    Scripted auto-commit on change (2024-02-16 12:20:58) by gitwatch.sh

--- 2024-02-16/lambdademo.py
+++ 2024-02-16/lambdademo.py
@@ -2,8 +2,9 @@ items = [('horse', 9), ('cow', 3), ('zebra', 1)]
 print(sorted(items,
              key=lambda t: t[1]))
 
-
-
-
-
-
+# Add one to every item in a list
+items = [9, 2, 1, 3, 5]
+new_items = items.copy()
+for value in items:
+    new_items.append(value+1)
+print(new_items)
