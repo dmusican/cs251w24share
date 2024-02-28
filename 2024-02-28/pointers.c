a0aeacd1c023f519cf32c8d7f43fa6310384bb05
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2024-02-28 11:24:52 -0600

    Scripted auto-commit on change (2024-02-28 11:24:52) by gitwatch.sh

--- 2024-02-28/pointers.c
+++ 2024-02-28/pointers.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+int add(int x, int y) {
+    return x + y;
+}
+
+int multiply(int x, int y) {
+    return x * y;
+}
+
+int doit(   f,   int x, int y) {
+    return f(x,y);
+}
+
 int main() {
     printf("Hi\n");
 }
