02736a8faac2f32e6571cf8635de66b3d5cc3887
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2024-02-28 12:14:46 -0600

    Scripted auto-commit on change (2024-02-28 12:14:46) by gitwatch.sh

--- 2024-02-28/Len2.java
+++ 2024-02-28/Len2.java
@@ -1,7 +1,9 @@
+import java.util.*;
 public class Len2 {
     public static void main(String[] args) {
         var x = 3;
         var y = x;
+        var scanner = new Scanner(System.in);
         System.out.println(y.length());
     }
 }
