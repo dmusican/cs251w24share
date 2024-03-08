63c74a5039a495e280a50b4a262a62e7641b3a9c
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2024-03-08 11:59:28 -0600

    Scripted auto-commit on change (2024-03-08 11:59:28) by gitwatch.sh

--- 2024-03-06/callby.py
+++ 2024-03-06/callby.py
@@ -4,19 +4,10 @@ class Thing:
 def foo(x):
     x = 6
 
-def moo(t):
-   t.x = 6;
-   t = Thing()
-   t.x = 7
-
 def main():
    a = 5
+   print(a)
    foo(a)
    print(a)
-
-   b = Thing()
-   b.x = 5
-   moo(b)
-   #print(b.x)
 
 main()
